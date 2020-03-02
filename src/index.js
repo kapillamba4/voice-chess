67f5fab7ef87afdd5888985e7657c040ada7b757
kapillamba4 <kapillamba4@gmail.com>
2020-03-02 19:38:07 +0530

    Rename Components to components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import App from './Components/App';
-import configureStore from './configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import App from "./components/App";
+import configureStore from "./configureStore";
 
 const store = configureStore();
-ReactDOM.render(<Provider store={store}>
-    <App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
