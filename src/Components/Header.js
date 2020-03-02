483c0f107c0e84411d57e3b0d1f24a8250d16534
kapillamba4 <kapillamba4@gmail.com>
2020-03-02 19:18:25 +0530

    Add Prettier

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,25 @@
-import React, {Component} from 'react';
-import './Header.scss';
-import {ReactComponent as GithubIcon} from '../icon-github.svg';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import "./Header.scss";
+import { ReactComponent as GithubIcon } from "../icon-github.svg";
+import { connect } from "react-redux";
 
 class Header extends Component {
-    render() {
-        return (<div className="page-header">
-            <div className="header-name">Voice Chess</div>
-            <div className="current-player">Current Player: {this.props.fen.split(" ")[1] === 'w' ? 'White': 'Black' }</div>
-            <GithubIcon className="github-icon" />
-            </div>);
-    }
+  render() {
+    return (
+      <div className="page-header">
+        <div className="header-name">Voice Chess</div>
+        <div className="current-player">
+          Current Player:{" "}
+          {this.props.fen.split(" ")[1] === "w" ? "White" : "Black"}
+        </div>
+        <a href="https://github.com/kapillamba4/voice-chess">
+          <GithubIcon className="github-icon" />
+        </a>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({ ...state.commands });
 
-export default connect(
-  mapStateToProps,
-  null,
-)(Header);
-
+export default connect(mapStateToProps, null)(Header);
--- src/actions/commands.js
+++ src/actions/commands.js
@@ -1,62 +1,135 @@
-import * as actionTypes from '../constants/actionTypes';
-import Chess from 'chess.js';
-import { CHESS_POSITIONS, MOVES } from '../constants/chessMoves'
+import * as actionTypes from "../constants/actionTypes";
+import Chess from "chess.js";
 
 const synth = window.speechSynthesis;
 
 export function initBoard() {
-    return dispatch => {
-        dispatch({ type: actionTypes.INIT_BOARD })
-    }
+  return dispatch => {
+    dispatch({ type: actionTypes.INIT_BOARD });
+  };
 }
-export function  makeMove(payload) {
-    return dispatch => {
-        dispatch({ type: actionTypes.MAKE_MOVE });
-        const chess = new Chess(payload.fen);
-        const olfFen = payload.fen;
-        chess.move({ from: payload.from, to: payload.to });
-        const fen = chess.fen();
-        if(chess.in_checkmate()){
+export function makeMove(payload) {
+  return dispatch => {
+    dispatch({ type: actionTypes.MAKE_MOVE });
+    const chess = new Chess(payload.fen);
+    const olfFen = payload.fen;
+    chess.move({ from: payload.from, to: payload.to });
+    const fen = chess.fen();
+    if (chess.in_checkmate()) {
+    } else if (olfFen === fen) {
+      dispatch({ type: actionTypes.MAKE_MOVE_FAILURE, payload });
+      if (synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+      }
 
-        }else if(olfFen === fen) {
-            dispatch({ type: actionTypes.MAKE_MOVE_FAILURE, payload });
-            if (synth.speaking) {
-                console.error('speechSynthesis.speaking');
-                return;
-            }
-            
-            let utterThis = new SpeechSynthesisUtterance('Failure');
-            utterThis.onend = function (event) {
-                console.log('SpeechSynthesisUtterance.onend');
-            }
-            utterThis.onerror = function (event) {
-                console.error('SpeechSynthesisUtterance.onerror');
-            }
+      let utterThis = new SpeechSynthesisUtterance("Failure");
+      utterThis.onend = function(event) {
+        console.log("SpeechSynthesisUtterance.onend");
+      };
+      utterThis.onerror = function(event) {
+        console.error("SpeechSynthesisUtterance.onerror");
+      };
 
-            synth.speak(utterThis);
-        }else{
-            if (synth.speaking) {
-                console.error('speechSynthesis.speaking');
-                return;
-            }
-            
-            let utterThis = new SpeechSynthesisUtterance(`Moved ${payload.from} to ${payload.to}`);
-            utterThis.onend = function (event) {
-                console.log('SpeechSynthesisUtterance.onend');
-            }
-            utterThis.onerror = function (event) {
-                console.error('SpeechSynthesisUtterance.onerror');
-            }
+      synth.speak(utterThis);
+    } else {
+      if (synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+      }
 
-            utterThis.rate = 1.2;
-            synth.speak(utterThis);
-            dispatch({ type: actionTypes.MAKE_MOVE_SUCCESS, payload: {...payload, fen, oldFen: payload.fen, prevFrom: payload.from, prevTo: payload.to} });
+      let utterThis = new SpeechSynthesisUtterance(
+        `Moved ${payload.from} to ${payload.to}`
+      );
+      utterThis.onend = function(event) {
+        console.log("SpeechSynthesisUtterance.onend");
+      };
+      utterThis.onerror = function(event) {
+        console.error("SpeechSynthesisUtterance.onerror");
+      };
+
+      utterThis.rate = 1.2;
+      synth.speak(utterThis);
+      const oldFenMap = payload.fen
+        .split(" ")[0]
+        .split("")
+        .filter(e => e !== "/")
+        .filter(e => e !== "8")
+        .filter(e => e !== "7")
+        .filter(e => e !== "6")
+        .filter(e => e !== "5")
+        .filter(e => e !== "4")
+        .filter(e => e !== "3")
+        .filter(e => e !== "2")
+        .filter(e => e !== "1")
+        .filter(e => e !== "0")
+        .reduce((acc, {}, idx, arr) => {
+          acc[arr[idx]] = acc[arr[idx]] === undefined ? 0 : acc[arr[idx]] + 1;
+          return acc;
+        }, {});
+      const newFenMap = fen
+        .split(" ")[0]
+        .split("")
+        .filter(e => e !== "/")
+        .filter(e => e !== "8")
+        .filter(e => e !== "7")
+        .filter(e => e !== "6")
+        .filter(e => e !== "5")
+        .filter(e => e !== "4")
+        .filter(e => e !== "3")
+        .filter(e => e !== "2")
+        .filter(e => e !== "1")
+        .filter(e => e !== "0")
+        .reduce((acc, {}, idx, arr) => {
+          acc[arr[idx]] = acc[arr[idx]] === undefined ? 0 : acc[arr[idx]] + 1;
+          return acc;
+        }, {});
+      const pieceName = {
+        r: "Black Rook",
+        n: "Black Knight",
+        b: "Black Bishop",
+        q: "Black Qween",
+        k: "Black King",
+        p: "Black Pawn",
+        R: "White Rook",
+        N: "White Knight",
+        B: "White Bishop",
+        Q: "White Qween",
+        K: "White King",
+        P: "White Pawn"
+      };
+      Object.keys(oldFenMap).forEach(val => {
+        if (newFenMap[val] !== oldFenMap[val]) {
+          utterThis = new SpeechSynthesisUtterance(
+            `${pieceName[val]} Eliminated`
+          );
+          utterThis.onend = function(event) {
+            console.log("SpeechSynthesisUtterance.onend");
+          };
+          utterThis.onerror = function(event) {
+            console.error("SpeechSynthesisUtterance.onerror");
+          };
+
+          utterThis.rate = 1.2;
+          synth.speak(utterThis);
         }
+      });
+      dispatch({
+        type: actionTypes.MAKE_MOVE_SUCCESS,
+        payload: {
+          ...payload,
+          fen,
+          oldFen: payload.fen,
+          prevFrom: payload.from,
+          prevTo: payload.to
+        }
+      });
     }
+  };
 }
 
 export function doUndo() {
-    return dispatch => {
-        dispatch({ type: actionTypes.DO_UNDO })
-    }
+  return dispatch => {
+    dispatch({ type: actionTypes.DO_UNDO });
+  };
 }
